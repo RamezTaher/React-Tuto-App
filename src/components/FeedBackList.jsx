475f6a3152b0f3f7468b29876689e2e59e265214
Ramez Taher <ramezgamer11@gmail.com>
2022-08-27 17:56:23 +0100

    animation added with frame-motion

--- src/components/FeedBackList.jsx
+++ src/components/FeedBackList.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import FeedBackItem from "./FeedBackItem"
 import PropTypes from "prop-types"
+import { motion, AnimatePresence } from "framer-motion"
 
 const FeedBackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
@@ -9,9 +10,22 @@ const FeedBackList = ({ feedback, handleDelete }) => {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedBackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedBackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
 }

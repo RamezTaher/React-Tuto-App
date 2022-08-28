fc5d17170fa044a7d4b381f1202ac0b2e800365f
Ramez Taher <ramezgamer11@gmail.com>
2022-08-28 14:34:33 +0100

    create and export the context

--- src/context/FeedBackContext.js
+++ src/context/FeedBackContext.js
@@ -1,9 +1,20 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 const FeedBackContext = createContext()
 
 export const FeedBackProvider = ({ children }) => {
+  const [feedBack, setFeedBack] = useState([
+    {
+      id: 1,
+      rating: 8,
+      text: " This Data from the Context",
+    },
+  ])
   return (
-    <FeedBackContext.Provider value={{}}>{children}</FeedBackContext.Provider>
+    <FeedBackContext.Provider value={{ feedBack }}>
+      {children}
+    </FeedBackContext.Provider>
   )
 }
+
+export default FeedBackContext
